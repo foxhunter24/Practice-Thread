5b15bf81238a4c193ce3297228a3a52fe608dce6
foxhunter24 <tmitul208@yahoo.com>
2015-03-28 22:23:47 +0600

    First Commit

---
+++ src/threadpractice/ThreadPractice.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadpractice;
+
+/**
+ *
+ * @author Mitul
+ */
+public class ThreadPractice extends Thread{
+    @Override
+    public void run(){
+        for( int i = 1; i <= 5; i++ ){
+            try{
+                Thread.sleep( 500 );
+            }catch( Exception ex ){
+                System.out.println( ex );
+            }
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Thread t1 = new Thread( new ThreadPractice() );
+        Thread t2 = new Thread( new ThreadPractice() );
+        Thread t3 = new Thread( new ThreadPractice() );
+        
+        t1.start();
+        //JOIN Method To be implemented
+        try {
+            // long seconds added 
+            t1.join( 1500 );
+        } catch ( InterruptedException ex ) {
+            System.out.println( ex );
+        }
+        
+        t2.start();
+        t3.start();
+        
+        
+        //Getting name of the thread
+//        System.out.println( t1.getName() );
+//        System.out.println( t2.getName() );
+//        System.out.println( t3.getName() );
+        
+        //Getting Id's
+//        
+//        System.out.println( t1.getId() );
+//        System.out.println( t2.getId() );
+//        System.out.println( t3.getId() );
+    }
+    
+}
